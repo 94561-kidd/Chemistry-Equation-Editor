687f66f3705adf0f5052e17ff9e76e5551b0a152
94561-kidd <94561aaaa@gmail.com>
2022-05-14 23:41:39 +0800

    upload the beta project (v0.1)

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function inputcode(x) {
+  let origin = document.getElementById("code").value;
+  document.getElementById("code").value = origin + x;
+}
+
+function copy() {
+  var content = `\u0024\\ce\u007B${
+    document.getElementById("code").value
+  }\u007D\u0024`;
+
+  navigator.clipboard
+    .writeText(content)
+    .then(() => {
+      console.log("內容已複製");
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
