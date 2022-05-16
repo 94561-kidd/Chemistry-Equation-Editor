dabedcac507c53c6a9530ca82eddc9148121961a
94561-kidd <94561aaaa@gmail.com>
2022-05-16 19:30:03 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 function inputcode(x) {
-  let origin = document.getElementById('code').value;
-  document.getElementById('code').value = origin + x;
+  let origin = document.getElementById("code").value;
+  document.getElementById("code").value = origin + x;
+
+  fix_double_blank();
 }
 
 function copy() {
   var content = `\u0024\\ce\u007B${
-    document.getElementById('code').value
+    document.getElementById("code").value
   }\u007D\u0024`;
 
   navigator.clipboard
@@ -16,4 +18,15 @@ function copy() {
     .catch((err) => {
       console.error(err);
     });
+
+  fix_double_blank();
+}
+
+function fix_double_blank() {
+  var str = document.getElementById("code").value;
+
+  while (str.indexOf("  ") != -1) {
+    str = str.replace("  ", " ");
+  }
+  document.getElementById("code").value = str;
 }
